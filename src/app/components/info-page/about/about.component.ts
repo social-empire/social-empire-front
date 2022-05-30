fda34b9cc5439c732e25ebee6276827c0f7c6802
JuniorPita <81571083+JuniorPita@users.noreply.github.com>
2022-05-30 16:01:20 +0300

    Added about section with some basic styles and scripts

--- src/app/components/info-page/about/about.component.ts
+++ src/app/components/info-page/about/about.component.ts
@@ -6,6 +6,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  public usersCounter: number = 0
+  public messagesCounter: number = 0
+  public likesCounter: number = 0
+  public languagesCounter: number = 0
 
   constructor() {}
 
